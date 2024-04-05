120bfe8049844bbd08234f87f5c2cc306b1cf2fd
Teo Lemane <teo.lemane@gmail.com>
2024-04-05 10:27:02 +0200

    add: abs

--- kmviz/core/utils.py
+++ kmviz/core/utils.py
@@ -18,7 +18,13 @@ def covyb_from_covyk(cov: Iterable[int], k: int, size: int) -> Tuple[float, List
             n += 1
             m += value
 
-    covyb = [x / k for x in covyb]
+    if cov[-1]:
+        n += (k-1)
+
+    begin = [covyb[x-1] / x for x in range(1, k)]
+    mid = [x / k for x in covyb[k-1:size-k+1]]
+    end = [covyb[x] / (k-(i)) for i, x in enumerate(range(size-k, size-1))]
+    covyb = begin + mid + end
 
     nk = size - k + 1
     return (m / nk), (n / size), sum(covyb) / size, covyb
