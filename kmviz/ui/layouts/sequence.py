ff8b3a44831c7a68c7f8c9dcb4b36abc2994ae5c
Teo Lemane <teo.lemane@gmail.com>
2024-07-03 14:47:39 +0200

    add: seq download cov + fig

--- kmviz/ui/layouts/sequence.py
+++ kmviz/ui/layouts/sequence.py
@@ -3,9 +3,13 @@ import dash_mantine_components as dmc
 from dash import no_update
 import plotly.express as px
 import plotly.graph_objects as go
+import pandas as pd
+import plotly.io as pio
+
+from dash_iconify import DashIconify
 
 from kmviz.ui.utils import make_select_data, prevent_update_on_none
-from kmviz.ui.components.figure import make_plot_title, make_plot_title_callbacks
+from kmviz.ui.components.figure import make_plot_title, make_plot_title_callbacks, make_select_input
 from kmviz.ui.components.select import kgsf
 from kmviz.ui.components.store import ksf
 from kmviz.ui.layouts.table import ktable
@@ -23,12 +27,40 @@ def blank_figure():
     return fig
 
 def make_sequence_layout():
+
+    down_select = make_select_input(
+        kseq.sid("down-select"),
+        label=None,
+        data=make_select_data(["png", "jpg", "svg", "pdf", "webp", "html", "json"]),
+        placeholder="format",
+        clearable=False,
+        icon=DashIconify(icon="material-symbols:download", width=20),
+        value="png",
+        searchable=True,
+    )
+
     res =  html.Div([
         dmc.Group([
             dmc.Select(
                 id=kseq.sid("select"),
                 label="Select ID"
-            )
+            ),
+            dmc.ActionIcon(
+                DashIconify(icon="bi:filetype-json", width=20),
+                id=kseq.sid("down-data-button"),
+                variant="filled",
+                color = "#1C7ED6"
+            ),
+            dcc.Download(id=kseq.sid("download-data")),
+            dmc.Group([
+                down_select,
+                dmc.ActionIcon(
+                    DashIconify(icon="material-symbols:download", width=20),
+                    id=kseq.sid("down-button"),
+                    variant="filled",
+                    color = "#1C7ED6",
+                ),
+            ], style = {"margin-left":"auto", "margin-right": 0, "width": "fit-content"}, spacing=5),
         ]),
 
         dcc.Graph(
@@ -39,11 +71,58 @@ def make_sequence_layout():
         ),
 
         make_plot_title(kseq.child("title")),
+
+        dcc.Download(id=kseq.sid("download")),
     ])
 
     return res
 
 def make_sequence_layout_callbacks():
+
+    @callback(
+        Input(kseq.sid("down-button"), "n_clicks"),
+        State(kseq.sid("down-select"), "value"),
+        State(kseq.sid("figure"), "figure"),
+        Output(kseq.sid("download"), "data")
+    )
+    def download_cov(n_clicks, fmt, data):
+        if n_clicks:
+            if fmt == "html":
+                content = pio.to_html(data, validate=False)
+                return dict(content=content, filename="kmviz-cov.html")
+            elif fmt == "json":
+                content = pio.to_json(data, validate=False)
+                return dict(content=content, filename="kmviz-cov.json")
+            else:
+                content = pio.to_image(data, fmt, validate=False)
+                return dcc.send_bytes(content, f"kmviz-cov.{fmt}")
+
+    @callback(
+        Input(kseq.sid("down-data-button"), "n_clicks"),
+        State(kgsf("provider"), "value"),
+        State(kgsf("query"), "value"),
+        State(ksf("query-results"), "data"),
+        Output(kseq.sid("download-data"), "data")
+    )
+    def download_json(n_clicks, provider, query, query_result):
+        if n_clicks:
+            if provider.startswith("__kmviz_df"):
+                prevent_update_on_none(None)
+
+            prevent_update_on_none(provider, query)
+
+            qr = query_result[query][provider]
+
+            res = {}
+            res[query] = {}
+
+            for sample, resp in qr.response.items():
+                if resp.has_abs():
+                    res[query][sample] = {"ykmer": resp.covyk, "ybase": resp.covyb}
+                else:
+                    res[query][sample] = {"xkmer": resp.covxk, "xbase": resp.covxb}
+
+            return dict(content=str(res), filename=f"{provider}-{query}.json")
 
     @callback(
         Input(kgsf("provider"), "value"),
@@ -98,10 +177,19 @@ def make_sequence_layout_callbacks():
 
         index = list(range(len(qr.query.seq)))
 
+        m = []
+        cols = ["base"]
+
+        if qr.response[sample].k:
+            m = [None] * (qr.response[sample].k-1)
+            cols = ["kmer", "base"]
+
         if qr.response[sample].has_abs():
-            fig = px.line(x=index, y=qr.response[sample].covyb, line_shape="linear")
+            df = pd.DataFrame({"Sequence": index, "kmer": qr.response[sample].covyk + m, "base": qr.response[sample].covyb})
+            fig = px.line(df, x="Sequence", y=cols, line_shape="linear", markers=True)
         else:
-            fig = px.line(x=index, y=qr.response[sample].covxb, line_shape="hv")
+            df = pd.DataFrame({"Sequence": index, "kmer": qr.response[sample].covxk + m, "base": qr.response[sample].covxb})
+            fig = px.line(df, x="Sequence", y=cols, line_shape="linear", markers=True)
 
         fig.update_layout(
             xaxis = dict(
@@ -110,11 +198,15 @@ def make_sequence_layout_callbacks():
                 ticktext = [x for x in qr.query.seq]
             )
         )
+
         fig.update_layout(
             xaxis=dict(rangeslider=dict(visible=True,range=[0, len(qr.query.seq)]),
                        range=[0, 50],
                        type="category")
         )
+
+        fig.update_layout(legend_title_text="")
+        fig.update_xaxes(title_text="Sequence")
 
         return fig
 
