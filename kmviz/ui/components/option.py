dc1ae3a247f142eb0afac368f5e7c4aedd9582ed
Teo Lemane <teo.lemane@gmail.com>
2024-10-31 17:45:15 +0100

    opt

--- kmviz/ui/components/option.py
+++ kmviz/ui/components/option.py
@@ -66,7 +66,6 @@ def make_multichoice_option(opt: MultiChoiceOption, id):
         className="kmviz-dmc-user-multi-select",
         clearable=True,
         searchable=True,
-        maxDropdownHeight=100,
         classNames={"root": "kmviz-dmc-select-input-root"},
         size="xs"
     )
